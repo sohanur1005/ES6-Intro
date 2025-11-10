e31bb0ccd888538bdb53825726c1e73141005d9e
sohanur1005 <sohanur.sr1005@gmail.com>
2025-11-10 09:43:22 +0600

    send this

--- let-var-const.js
+++ let-var-const.js
@@ -1,3 +1,14 @@
 // var: no reason to use
 // let:allow it to reassign
 // const:don`t allow it to reassign
+
+/*TypeError: Assignment to constant variable.
+const money=50;
+money=60;
+console.log(money)*/
+
+let count=10;
+count=count+10;
+console.log(count)
+
+
