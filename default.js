273d99e25da7ebac46744407ae82403967eca661
sohanur1005 <sohanur.sr1005@gmail.com>
2025-11-14 22:56:14 +0600

    send this

---
+++ default.js
@@ -0,0 +1,10 @@
+function add(num1, num2=0)
+{
+    const result =num1+num2;
+    console.log(num1,'+', num2,'=' , result);
+    return result;
+}
+
+// const sum=add(5,10);
+const sum2=add(5);
+// const sum3=add(5,10);
