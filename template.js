1f36fe3d6d66f85e7357418751dd5f73aa09d26a
sohanur1005 <sohanur.sr1005@gmail.com>
2025-11-14 23:42:36 +0600

    send this

--- template.js
+++ template.js
@@ -1,6 +1,17 @@
 const a=10;
 const b=10;
+const num=[45,7,8,4];
+const student={name:'Sohanur Rahman',age:24}
 
-// `` =Backtrack sign
+
+
+// `` =Backtick sign power
+const sum1=`The sum of ${a} and ${num[1]} and ${student.age} is ${a+num[1]+student.age}`
 const sum=`The sum of ${a} and ${b} is ${a+b}`
+console.log(sum1)
 console.log(sum)
+
+const name=`Sohanur
+Rahman
+Sohag`;
+console.log(name)
