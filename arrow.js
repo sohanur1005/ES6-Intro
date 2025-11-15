c451e01635a478e69d109e62c22e35f57f66f92c
sohanur1005 <sohanur.sr1005@gmail.com>
2025-11-15 23:13:32 +0600

    send this

--- arrow.js
+++ arrow.js
@@ -28,6 +28,12 @@ console.log(sum2)
  const add3=(a,b) => a+b;
  const sum3=add3(10,10);
  console.log(sum3)
+//  multiple parameter
+ 
+ const add4=(n1,n2,n3,n4) => (n1+n2)*(n3+n4);
+ const multi=add4(1,2,3,4)
+ 
+ console.log(multi)
 
 
 
