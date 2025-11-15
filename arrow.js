8785f9c3fa1dbc93c0c402208c7e03dda28fb1af
sohanur1005 <sohanur.sr1005@gmail.com>
2025-11-15 22:34:14 +0600

    send this

---
+++ arrow.js
@@ -0,0 +1,19 @@
+
+// function decleration
+function add(a,b)
+{
+    const result=a+b;
+    return result;
+}
+
+// function expression
+
+function add2(a,b)
+{
+    return a+b;
+}
+const sum=add(10,10)
+const sum2=add2(10,10)
+console.log(sum)
+console.log(sum2)
+
