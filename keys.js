5f3ed367aea192b978f49cf8267e0e00d63ae7b4
sohanur1005 <sohanur.sr1005@gmail.com>
2025-11-20 15:37:12 +0600

    send this

---
+++ keys.js
@@ -0,0 +1,16 @@
+const glass={name:'glass',
+    color:'golden',
+    price:10,
+    isCleaned:true
+};
+console.log( glass)
+// delete a object
+delete glass.isCleaned;
+console.log(glass)
+// show object property/keys name
+const keys=Object.keys(glass)
+console.log(keys)
+
+// show keys values
+const values=Object.values(glass)
+console.log(values)
