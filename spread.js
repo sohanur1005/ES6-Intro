be707ff3f9ed727a295c7de5d1cccee2d38a1d8c
sohanur1005 <sohanur.sr1005@gmail.com>
2025-11-20 13:41:22 +0600

    send this

---
+++ spread.js
@@ -0,0 +1,8 @@
+const max=Math.max(1,8,45,75,98,32)
+console.log(max)
+
+const numbers=[4,87,52,32,65,41,5];
+// const arrayMax=Math.max(numbers)
+// using ...(soread up operator) the arry will convert numbers
+const arrayMax=Math.max(...numbers)
+console.log(arrayMax)
