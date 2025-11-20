05c7d042aa891b2aca8e9f4721f30a108307d44c
sohanur1005 <sohanur.sr1005@gmail.com>
2025-11-20 12:01:27 +0600

    send this

---
+++ more-arrow.js
@@ -0,0 +1,10 @@
+const getAge=(person)=>person.age;
+const student={name:'sohanur',age:24}
+const age=getAge(student)
+console.log(age)
+
+
+const getThird=numbers=>numbers[2];
+const arr=[1,2,3,4,5,6,7];
+const third=getThird(arr)
+console.log(third)
